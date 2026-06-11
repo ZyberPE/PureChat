dd6ced2180ee624cacbd46f2584055bcd7b58677
ZyberPE <zyberpe@gmail.com>
2026-06-10 23:54:58 -0400

    Update PureChat.php
    Signed-off-by: ZyberPE <zyberpe@gmail.com>

--- src/_64FF00/PureChat/PureChat.php
+++ src/_64FF00/PureChat/PureChat.php
@@ -50,14 +50,9 @@ class PureChat extends PluginBase
  |  __/  | |_| | | |    |  __/ | |___  | | | | | (_| | | |_ 
  |_|      \__,_| |_|     \___|  \____| |_| |_|  \__,_|  \__|
         by fernanACM and _64FF00 - https://github.com/fernanACM
-case "zfactions":
-    if($this->getServer()->getPluginManager()->getPlugin("zFactions") !== null){
-        $this->factionsAPI = new zFactions();
-        $this->getLogger()->notice("zFactions support enabled.");
-        break;
-    }
-    $this->getLogger()->notice("zFactions was not found.");
-break;
+                                                             ");
+        $this->loadFactionsPlugin();
+        $this->getServer()->getPluginManager()->registerEvents(new PCListener($this), $this);
 
     /**
      * @return void
